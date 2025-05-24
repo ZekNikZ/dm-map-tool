803d1807edbd452b37c315b2f8250c58ebb8cd3a
Matthew McCaskill <mattrmccaskill@gmail.com>
2025-05-24 01:49:38 -0500

    Make map display readjust to center when resizing window

--- src/components/MapDisplay.tsx
+++ src/components/MapDisplay.tsx
@@ -2,7 +2,10 @@
 
 import type { Map } from "@/types/structure";
 import { useEffect, useMemo, useRef } from "react";
-import Panzoom from "@panzoom/panzoom";
+import Panzoom, {
+  type PanzoomEventDetail,
+  type PanzoomObject,
+} from "@panzoom/panzoom";
 import { LayerComponent } from "./layers/LayerComponent";
 import type { MapContext } from "@/types/rendering";
 
@@ -10,23 +13,72 @@ interface Props {
   map: Map;
 }
 
+interface TrackedRect {
+  width: number;
+  height: number;
+}
+
 export default function MapDisplay({ map }: Props) {
   const ref = useRef<HTMLDivElement>(null);
+  const panzoomRef = useRef<PanzoomObject | null>(null);
+  const elementBoundingBoxRef = useRef<TrackedRect | null>(null);
+
+  function onResize() {
+    // If panzoom is not initialized, do nothing
+    const panzoom = panzoomRef.current;
+    const element = ref.current;
+    if (!panzoom || !element) {
+      return;
+    }
+
+    // Calculate the new bounding box
+    const oldWidth = elementBoundingBoxRef.current?.width ?? 0;
+    const oldHeight = elementBoundingBoxRef.current?.height ?? 0;
+
+    const newWidth = element.clientHeight;
+    const newHeight = element.clientWidth;
+
+    const diffX = newWidth - oldWidth;
+    const diffY = newHeight - oldHeight;
+
+    elementBoundingBoxRef.current = {
+      width: newWidth,
+      height: newHeight,
+    };
+
+    // Move the center of the panzoom to the center of the new bounding box
+    panzoom.pan(diffY / 2, diffX / 2, { relative: true, animate: false });
+  }
 
   useEffect(() => {
     if (!ref.current) {
       return;
     }
 
+    // Create panzoom
     const element = ref.current;
     const panzoom = Panzoom(element, {
       animate: true,
       easing: "ease-in-out",
       overflow: "hidden",
     });
+    panzoomRef.current = panzoom;
+
+    // Add events
     element.parentElement?.addEventListener("wheel", panzoom.zoomWithWheel);
+    window.addEventListener("resize", onResize);
+
+    // Initial resize
+    elementBoundingBoxRef.current = {
+      width: element.clientWidth,
+      height: element.clientHeight,
+    };
+
+    // Clean up
     return () => {
       panzoom.destroy();
+      panzoomRef.current = null;
+      window.removeEventListener("resize", onResize);
     };
   }, [ref]);
 
@@ -40,7 +92,7 @@ export default function MapDisplay({ map }: Props) {
   return (
     <div
       ref={ref}
-      className="relative w-full"
+      className="relative h-full w-full"
       onContextMenu={(event) => event.preventDefault()}
     >
       {map.layers.length > 0 && (
